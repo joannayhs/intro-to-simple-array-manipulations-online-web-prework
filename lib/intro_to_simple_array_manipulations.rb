8c9dd20ed1a5948ad8fbaafdb650b32eac94c748
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-20 19:37:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,8 +3,7 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
+  array.unshift("Brooklyn Heights")
 end
 
 def using_pop(array)
