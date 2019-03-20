eecd874a7df839a091193a701b8eee7890cb5443
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-20 19:36:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,5 @@
 def using_push(array, string)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  countries_in_western_africa.push("Niger")
+  array.push("string")
 end
 
 def using_unshift(array, string)
