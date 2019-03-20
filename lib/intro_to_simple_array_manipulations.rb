5cc1b603900139e832164114ccdc911db9923183
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-20 19:14:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,3 +17,7 @@ def pop_with_args(array)
   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
   chars_in_game_of_thrones.pop(2)
 end
+
+def using_shift(array)
+  
+end
